05c9bf8cb169b9e5b6d3ea0faf3b0c1bd61ff5ef
gartee-john-PFG <gartee.john@principal.com>
2021-03-10 12:51:40 -0500

    clean up for code-server

--- VideoStore/lib/src/test/java/VideoStore/VideoStoreTests.java
+++ VideoStore/lib/src/test/java/VideoStore/VideoStoreTests.java
@@ -1,7 +1,10 @@
 package VideoStore;
 
-import org.junit.jupiter.api.Test;
+import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VideoStoreTests {
     @Test
