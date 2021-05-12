8a48553d2d9a9ca88c88e479b38379e1e4f7d2e4
John Gartee <gartee.john@principal.com>
2021-05-12 10:57:39 -0400

    fix VideoStore build

--- VideoStore/src/test/java/VideoStore/VideoStoreTests.java
+++ VideoStore/src/test/java/VideoStore/VideoStoreTests.java
@@ -9,5 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VideoStoreTests {
     @Test
     public void approveCustomerStatement() {
+        Customer cust = new Customer("John Gartee");
+        cust.addRental(new Rental(new Movie("Star Wars", Movie.REGULAR), 3));
+
+        String result = cust.statement();
+
+        Approvals.verify(result);
+        
     }
 }
